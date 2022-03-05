cf80270adb7564a01be3f3e6d949910c4981ebbe
Eric Spencer <spencer22eric@gmail.com>
2022-03-04 20:57:49 -0500

    defining variables and adding base html

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+let myQuestions = [
+    {
+        question: "Commonly used data types do NOT include:_______",
+        answers: {
+            a: 'strings',
+            b: 'booleans',
+            c: 'alerts',
+            d: 'numbers'
+        },
+
+        correctAnswer: 'a'
+    },
+
+    {
+        question: "The condition of an if/else statement is enclosed with:____",
+        answers: {
+            a: 'quotes',
+            b: 'curly brackets',
+            c: 'parenthesis',
+            d: 'square brackets'
+        },
+
+        correctAnswer: 'c'
+    },
+
+    {
+        question: "Arrays in JavaScript can be used to store:_____",
+        answers: {
+            a: 'numbers and strings',
+            b: 'other arrays',
+            c: 'booleans',
+            d: 'all of the above'
+        },
+
+        correctAnswer: 'd'
+    },
+
+    {
+        question: "String values must be enclosed with ______ when being assigned to variables",
+        answers: {
+            a: 'commas',
+            b: 'curly brackets',
+            c: 'quotes',
+            d: 'parenthesis'
+        },
+
+        correctAnswer: 'c'
+    }
+]
+
+// function showQuestions(questions, quizContainer) {
+
+//     let output = [];
+//     let answers;
+
+//     for (let i=0; i < questions.length; i++) {
+
+//         answers = [];
+//     }
+// }
